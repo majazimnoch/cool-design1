83865b12ba4923a6e582834e6828b864bb4b9759
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2024-07-29 21:35:10 +0200

    added a new component navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+
+const NavbarWrapper = styled.div`
+background-color: red;
+border: 1px blue solid;
+`
+const Navbar = () => {
+    return (
+        <div>
+           This is a navbar
+        </div>
+    )
+}
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import styled from "styled-components";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const  StyledH = styled.h1`
 color: red;
@@ -11,6 +12,7 @@ color: red;
 export default function Home() {
   return (
     <>
+    <Navbar />
     <Hero />
        <StyledH>Hej</StyledH>
     </>
